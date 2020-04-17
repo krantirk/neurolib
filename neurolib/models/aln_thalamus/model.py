fixed delay problems and computation of max delay and startindex

neurolib/models/aln_thalamus/model.py
@@ -1,8 +1,10 @@
 
 
+import numpy as np
+
+from ..model import Model
 from . import loadDefaultParams as dp
 from . import timeIntegration as ti
-from ..model import Model
 
 
 class ALNThalamusModel(Model):
@@ -87,7 +89,7 @@ class ALNThalamusModel(Model):
     input_vars = ["ext_exc_current", "ext_exc_rate"]
     default_input = "ext_exc_rate"
 
-    def __init__(self, params=None, Cmat=None, Dmat=None, lookupTableFileName=None, seed=None):
+    def __init__(self, params=None, Cmat=None, Dmat=None, thlm_cmat=None, thlm_dmat=None, lookupTableFileName=None, seed=None):
         """
         :param params: parameter dictionary of the model
         :param Cmat: Global connectivity matrix (connects E to E)
@@ -106,7 +108,7 @@ class ALNThalamusModel(Model):
 
         if params is None:
             params = dp.loadDefaultParams(
-                Cmat=self.Cmat, Dmat=self.Dmat, lookupTableFileName=self.lookupTableFileName, seed=self.seed
+                Cmat=self.Cmat, Dmat=self.Dmat, thlm_cmat=thlm_cmat, thlm_dmat=thlm_dmat, lookupTableFileName=self.lookupTableFileName, seed=self.seed
             )
 
         super().__init__(integration=integration, params=params)
@@ -115,5 +117,7 @@ class ALNThalamusModel(Model):
         # compute maximum delay of model
         ndt_de = round(self.params["de"] / self.params["dt"])
         ndt_di = round(self.params["di"] / self.params["dt"])
+        thlm_max_delay = np.around(self.params["thlm_dmat"] / self.params["dt"]).max()
+        ctx_thlm_delay = round(self.params["ctx_thlm_delay"] / self.params["dt"])
         max_dmat_delay = super().getMaxDelay()
-        return int(max(max_dmat_delay, ndt_de, ndt_di))
+        return int(max(max_dmat_delay, ndt_de, ndt_di, thlm_max_delay, ctx_thlm_delay))

neurolib/models/aln_thalamus/timeIntegration.py
@@ -212,7 +212,7 @@ def timeIntegration(params):
     # for l in range(N):
     #    Dmat_ndt[l, l] = ndt_de  # if no distributed, this is a fixed value (E-E coupling)
 
-    max_global_delay = max(np.max(Dmat_ndt), ndt_de, ndt_di)
+    max_global_delay = max(np.max(Dmat_ndt), ndt_de, ndt_di, np.max(thlm_dmat_ndt), ndt_ctx_thlm_delay)
     startind = int(max_global_delay + 1)
 
     # state variable arrays, have length of t + startind